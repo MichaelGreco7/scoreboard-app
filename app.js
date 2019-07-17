b5738217da0b81bdb77851448511076b2c91d715
Michael Greco <michael.greco07@gmail.com>
2019-07-17 15:33:41 -0400

    1st real commit

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
-const Header = () => {
-    return (
-        <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
-        </header>
-    );
-}
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      <span className="stats">Players: { props.totalPlayers }</span>
+    </header>
+  );
+};
 
 const Player = () => {
-    return (
-        <div className="player">
-            <span className="player-name">
-                Michael
-            </span>
-            <Counter />
-        </div>
-    )
-}
+  return (
+    <div className="player">
+      <span className="player-name">Michael</span>
+      <Counter />
+    </div>
+  );
+};
 
 const Counter = () => {
-    return (
-        <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score">35</span>
-                <button className="counter-action increment"> + </button>
-            </div>
-    );
-}
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
 
 const App = () => {
-    return (
-        <div className="scoreboard">
-            <Header />
+  return (
+    <div className="scoreboard">
+      <Header title="Scoreboard" totalPlayers={1} />
 
-            {/* Players list */}
-            <Player />
-        </div>
-    );
-}
+      {/* Players list */}
+      <Player />
+    </div>
+  );
+};
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
